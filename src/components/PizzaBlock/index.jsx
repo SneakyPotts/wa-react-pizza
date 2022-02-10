8a032468a0a94b33c88bb2ae6e8b4ae913a527eb
unknown <you@example.com>
2022-02-10 19:53:31 +0200

    added dinamic card render

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import classNames from "classnames";
 
-const PizzaBlock = () => {
+const PizzaBlock = ({ data }) => {
+  const {
+    title,
+    img,
+    type,
+    radius,
+    price
+  } = data;
+
+  const [conf, setConf] = useState({
+    type: 1,
+    radius: 1
+  });
+
+  const changeConf = (name, value) => {
+    return () => {
+      setConf({
+        ...conf,
+        [name]: value
+      });
+    }
+  }
+
   return (
     <div className="pizza-block">
       <img
         className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+        src={img}
         alt="Pizza"
       />
-      <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+      <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {type?.map((i, index) => (
+            <li
+              key={i?.type}
+              className={classNames({active: (i?.type === conf?.type)})}
+              onClick={changeConf('type', i?.type)}
+            >{i?.title}</li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.
-          </li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {radius?.map((i, index) => (
+            <li
+              key={i?.type}
+              className={classNames({active: i?.type === conf?.radius})}
+              onClick={changeConf('radius', i?.type)}
+            >{i?.title}</li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от 395 ₽</div>
+        <div className="pizza-block__price">от {price} ₽</div>
         <div className="button button--outline button--add">
           <svg
             width="12"
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 import Categories from "../../components/Categories";
 import Sort from "../../components/Sort";
 import PizzaBlock from "../../components/PizzaBlock";
 import {useEffect} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getData} from "../../store/actions/home";
 import DataService from "../../api/DataService";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const { data } = useSelector(state => state.home);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect( async () => {
     const data = await DataService.fetchData();
     dispatch(getData(data));
+    setIsLoading(false)
   }, [])
 
   return (
@@ -24,15 +28,17 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        <PizzaBlock/>
-        <PizzaBlock/>
-        <PizzaBlock/>
-        <PizzaBlock/>
-        <PizzaBlock/>
-        <PizzaBlock/>
-        <PizzaBlock/>
-        <PizzaBlock/>
-        <PizzaBlock/>
+        { isLoading
+          ? (
+            <p>Загрузка...</p>
+          )
+          : data?.map((i, index) => (
+          <PizzaBlock
+            key={index}
+            data={i}
+          />
+
+        ))}
       </div>
     </>
   );
